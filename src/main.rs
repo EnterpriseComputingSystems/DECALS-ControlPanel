252f6de933a50fa1f7328aa33e4f69efb7ae8e44
carlyrobison <carly.ellen.robison@gmail.com>
2018-01-23 01:01:22 -0800

    Add graphics imports

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,17 @@
 extern crate DECALS_base;
 use DECALS_base::{Network};
 
+extern crate piston;
+extern crate graphics;
+extern crate glutin_window;
+extern crate opengl_graphics;
+
+use piston::window::WindowSettings;
+use piston::event_loop::*;
+use piston::input::*;
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{ GlGraphics, OpenGL };
+
 
 fn main() {
 
@@ -11,5 +22,4 @@ fn main() {
     let net: Network = Network::new(interests);
 
     println!("{}", net.get_num_devices());
-
 }
